8d14db0df79c6e134419fd01ff3ee6deae361174
Gaming32 <soujournme@gmail.com>
2021-01-08 12:00:41 -0600

    finished grailLazyStableSort

--- grailsort.pyx
+++ grailsort.pyx
@@ -62,6 +62,90 @@ cdef class GrailSort:
             else:
                 right = middle
         return left
+
+    @staticmethod
+    cdef int grailBinarySearchRight(list array, int start, int length, object target):
+        cdef int left = 0
+        cdef int right = length
+
+        cdef int middle
+        while left < right:
+            middle = left + ((right - left) // 2)
+
+            if array[start + middle] > target:
+                right = middle
+            else:
+                left = middle + 1
+        return right
+
+    @staticmethod
+    cdef void grailLazyMerge(list array, int start, int leftLen, int rightLen):
+        cdef int middle, mergeLen, end
+        if leftLen < rightLen:
+            middle = start + leftLen
+
+            while leftLen != 0:
+                mergeLen = GrailSort.grailBinarySearchLeft(array, middle, rightLen, array[start])
+
+                if mergeLen != 0:
+                    GrailSort.grailRotate(array, start, leftLen, mergeLen)
+                    start += mergeLen
+                    rightLen -= mergeLen
+                
+                middle += mergeLen
+
+                if rightLen == 0:
+                    break
+                else:
+                    while True:
+                        start += 1
+                        leftLen -= 1
+                        if not (leftLen != 0 and array[start] <= array[middle]):
+                            break
+        else:
+            end = start + leftLen + rightLen - 1
+
+            while rightLen != 0:
+                mergeLen = GrailSort.grailBinarySearchRight(array, start, leftLen, array[end])
+
+                if mergeLen != leftLen:
+                    GrailSort.grailRotate(array, start + mergeLen, leftLen - mergeLen, rightLen)
+                    end -= leftLen - mergeLen
+                    leftLen = mergeLen
+                
+                if leftLen == 0:
+                    break
+                else:
+                    middle = start + leftLen
+                    while True:
+                        rightLen -= 1
+                        end -= 1
+                        if not (rightLen != 0 and array[middle - 1] <= array[end]):
+                            break
+
+    @staticmethod
+    cdef void grailLazyStableSort(list array, int start, int length):
+        cdef int index, left, right
+        for index in range(1, length, 2):
+            left = start + index - 1
+            right = start + index
+
+            if array[left] > array[right]:
+                GrailSort.grailSwap(array, left, right)
+        cdef int mergeLen, fullMerge, mergeIndex, mergeEnd, leftOver
+        mergeLen = 2
+        while mergeLen < length:
+            fullMerge = 2 * mergeLen
+
+            mergeEnd = length - fullMerge   
+
+            for mergeIndex from 0 <= mergeIndex <= mergeEnd by fullMerge:
+                GrailSort.grailLazyMerge(array, start + mergeIndex, mergeLen, mergeLen)
+
+            leftOver = length - mergeIndex
+            if leftOver > mergeLen:
+                GrailSort.grailLazyMerge(array, start + mergeIndex, mergeLen, leftOver - mergeLen)
+            mergeLen *= 2
     
     @staticmethod
     cdef int grailCollectKeys(list array, int start, int length, int idealKeys):
@@ -101,3 +185,18 @@ cdef class GrailSort:
         cdef int idealKeys = keyLen + blockLen
 
         cdef int keysFound = GrailSort.grailCollectKeys(array, start, length, idealKeys)
+
+        cdef bint idealBuffer
+        if keysFound < idealKeys:
+            if keysFound < 4:
+                GrailSort.grailLazyStableSort(array, start, length)
+                return
+            else:
+                keyLen = blockLen
+                blockLen = 0
+                idealBuffer = False
+
+                while keyLen > keysFound:
+                    keyLen //= 2
+        else:
+            idealBuffer = True
--- test.py
+++ test.py
@@ -7,4 +7,14 @@ LENGTH = 4096
 l = [random.randrange(LENGTH) for _ in range(LENGTH)]
 gs = GrailSort()
 gs.grailCommonSort(l, 0, LENGTH, None, 0)
-print(all(l[i - 1] <= l[i] for i in range(1, LENGTH)))
+
+broke = None
+for i in range(1, LENGTH):
+    if l[i - 1] > l[i]:
+        broke = i - 1
+        break
+
+if broke is None:
+    print('The array was sorted!')
+else:
+    print('Indices', broke, 'and', broke + 1, 'were out of order!')
